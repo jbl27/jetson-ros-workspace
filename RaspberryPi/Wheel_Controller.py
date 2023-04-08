db599684a7180de38d5cefb5a7b57073c4410b79
ArnoldYing <ArnoldYing@users.noreply.github.com>
2023-04-08 20:19:31 +0000

    autopep8 action fixes

--- RaspberryPi/Wheel_Controller.py
+++ RaspberryPi/Wheel_Controller.py
@@ -11,6 +11,7 @@ current_angle = 0.0
 movement_arr = [V_Directions.halted, H_Directions.straight]
 pwm_controls = []
 
+
 def run_algorithm(alg, vid_file):
     vid = cv.VideoCapture(vid_file)
 
@@ -35,8 +36,10 @@ def run_algorithm(alg, vid_file):
         if key == 27:
             break
 
+
 def main():
-  setup_rpi(pwm_controls=pwm_controls)
-  run_algorithm(Seesaw, "../Navigation/videos/down1.mp4")
-    
+    setup_rpi(pwm_controls=pwm_controls)
+    run_algorithm(Seesaw, "../Navigation/videos/down1.mp4")
+
+
 main()
